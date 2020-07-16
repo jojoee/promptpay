68ea544b70ef7fdbf07f4b19ee0374aaa9ea5026
Nathachai Thongniran <inid3a@gmail.com>
2020-07-16 15:25:58 +0700

    change unittest to pytest

--- tests/test_qrcode.py
+++ tests/test_qrcode.py
@@ -1,12 +1,5 @@
-import unittest
 from promptpay import qrcode
 
 
-class TestSum(unittest.TestCase):
-
-    def test_e(self):
-        self.assertEqual(qrcode.generate_payload(3), "eee", "Should be generate payload")
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_qrcode():
+    assert qrcode.generate_payload(3) == "eee"
