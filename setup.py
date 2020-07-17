3d3a4d68f393705631107695457fb09fd87dad25
Nathachai Thongniran <inid3a@gmail.com>
2020-07-17 15:40:42 +0700

    version, bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="promptpay",
-    version="0.0.2",
+    version="1.0.0",
     description="Python library to generate PromptPay QR Code",
     long_description=README,
     long_description_content_type="text/markdown",
