d3d395ac5584f0727f51e0ed9b0f92e09ac4385b
Nathachai Thongniran <inid3a@gmail.com>
2020-07-18 12:06:08 +0700

    style, format code with black

--- promptpay/qrcode.py
+++ promptpay/qrcode.py
@@ -2,28 +2,28 @@ import re
 import crc16
 import qrcode
 
-ID_PAYLOAD_FORMAT = '00'
-ID_POI_METHOD = '01'
-ID_MERCHANT_INFORMATION_BOT = '29'
-ID_TRANSACTION_CURRENCY = '53'
-ID_TRANSACTION_AMOUNT = '54'
-ID_COUNTRY_CODE = '58'
-ID_CRC = '63'
+ID_PAYLOAD_FORMAT = "00"
+ID_POI_METHOD = "01"
+ID_MERCHANT_INFORMATION_BOT = "29"
+ID_TRANSACTION_CURRENCY = "53"
+ID_TRANSACTION_AMOUNT = "54"
+ID_COUNTRY_CODE = "58"
+ID_CRC = "63"
 
-PAYLOAD_FORMAT_EMV_QRCPS_MERCHANT_PRESENTED_MODE = '01'
-POI_METHOD_STATIC = '11'
-POI_METHOD_DYNAMIC = '12'
-MERCHANT_INFORMATION_TEMPLATE_ID_GUID = '00'
-BOT_ID_MERCHANT_PHONE_NUMBER = '01'
-BOT_ID_MERCHANT_TAX_ID = '02'
-BOT_ID_MERCHANT_EWALLET_ID = '03'
-GUID_PROMPTPAY = 'A000000677010111'
-TRANSACTION_CURRENCY_THB = '764'
-COUNTRY_CODE_TH = 'TH'
+PAYLOAD_FORMAT_EMV_QRCPS_MERCHANT_PRESENTED_MODE = "01"
+POI_METHOD_STATIC = "11"
+POI_METHOD_DYNAMIC = "12"
+MERCHANT_INFORMATION_TEMPLATE_ID_GUID = "00"
+BOT_ID_MERCHANT_PHONE_NUMBER = "01"
+BOT_ID_MERCHANT_TAX_ID = "02"
+BOT_ID_MERCHANT_EWALLET_ID = "03"
+GUID_PROMPTPAY = "A000000677010111"
+TRANSACTION_CURRENCY_THB = "764"
+COUNTRY_CODE_TH = "TH"
 
 
 def sanitize_target(target=""):
-    result = re.sub(r'\D', '', target)
+    result = re.sub(r"\D", "", target)
 
     return result
 
@@ -32,29 +32,29 @@ def format_target(target=""):
     result = sanitize_target(target)
 
     if len(result) < 13:
-        result = re.sub('^0', '66', result)
-        result = ('0000000000000' + result)[-13:]  # last 13 digits
+        result = re.sub("^0", "66", result)
+        result = ("0000000000000" + result)[-13:]  # last 13 digits
 
     return result
 
 
 def format_amount(amount=0):
-    result = '{0:.2f}'.format(amount)
+    result = "{0:.2f}".format(amount)
 
     return result
 
 
 def checksum(target=""):
-    byte_str = target.encode('ascii')  # convert to bytes
-    hex_str = hex(crc16.crc16xmodem(byte_str, 0xffff))
-    code = hex_str.replace('0x', '').upper()
-    result = ('0000' + code)[-4:]  # only last 4 digits
+    byte_str = target.encode("ascii")  # convert to bytes
+    hex_str = hex(crc16.crc16xmodem(byte_str, 0xFFFF))
+    code = hex_str.replace("0x", "").upper()
+    result = ("0000" + code)[-4:]  # only last 4 digits
 
     return result
 
 
 def format(id="", value=""):
-    last2digits = ('00' + str(len(value)))[-2:]
+    last2digits = ("00" + str(len(value)))[-2:]
     result = id + last2digits + value
 
     return result
@@ -75,21 +75,25 @@ def generate_payload(id="", amount=0):
     data = [
         format(ID_PAYLOAD_FORMAT, PAYLOAD_FORMAT_EMV_QRCPS_MERCHANT_PRESENTED_MODE),
         format(ID_POI_METHOD, POI_METHOD_DYNAMIC if amount else POI_METHOD_STATIC),
-        format(ID_MERCHANT_INFORMATION_BOT, ''.join([
-            format(MERCHANT_INFORMATION_TEMPLATE_ID_GUID, GUID_PROMPTPAY),
-            format(target_type, format_target(target))
-        ])),
+        format(
+            ID_MERCHANT_INFORMATION_BOT,
+            "".join(
+                [
+                    format(MERCHANT_INFORMATION_TEMPLATE_ID_GUID, GUID_PROMPTPAY),
+                    format(target_type, format_target(target)),
+                ]
+            ),
+        ),
         format(ID_COUNTRY_CODE, COUNTRY_CODE_TH),
-        format(ID_TRANSACTION_CURRENCY, TRANSACTION_CURRENCY_THB)
-        # amount & &
+        format(ID_TRANSACTION_CURRENCY, TRANSACTION_CURRENCY_THB),
     ]
     if amount:
         data.append(format(ID_TRANSACTION_AMOUNT, format_amount(amount)))
 
-    data2crc = ''.join(data) + ID_CRC + '04'
+    data2crc = "".join(data) + ID_CRC + "04"
     data.append(format(ID_CRC, checksum(data2crc)))
 
-    return ''.join(data)
+    return "".join(data)
 
 
 def to_image(payload=""):
@@ -98,7 +102,7 @@ def to_image(payload=""):
 
 
 def to_file(payload="", filepath=""):
-    imgfile = open(filepath, 'wb')
+    imgfile = open(filepath, "wb")
     img = to_image(payload)
-    img.save(imgfile, 'PNG')
+    img.save(imgfile, "PNG")
     imgfile.close()
--- tests/test_qrcode.py
+++ tests/test_qrcode.py
@@ -3,25 +3,24 @@ import os.path
 
 
 class TestSanitizeTarget:
-
     def test_normal(self):
         # phone
-        assert qrcode.sanitize_target(target='0801234567') == "0801234567"
+        assert qrcode.sanitize_target(target="0801234567") == "0801234567"
         # national id
-        assert qrcode.sanitize_target(target='1111111111111') == "1111111111111"
+        assert qrcode.sanitize_target(target="1111111111111") == "1111111111111"
         # tax id
-        assert qrcode.sanitize_target(target='0123456789012') == "0123456789012"
+        assert qrcode.sanitize_target(target="0123456789012") == "0123456789012"
         # ewallet
-        assert qrcode.sanitize_target(target='012345678901234') == "012345678901234"
+        assert qrcode.sanitize_target(target="012345678901234") == "012345678901234"
 
     def test_with_dash(self):
         # phone
-        assert qrcode.sanitize_target(target='080-123-4567') == "0801234567"
+        assert qrcode.sanitize_target(target="080-123-4567") == "0801234567"
         # national id
-        assert qrcode.sanitize_target(target='1-1111-11111-11-1') == "1111111111111"
+        assert qrcode.sanitize_target(target="1-1111-11111-11-1") == "1111111111111"
 
     def test_with_international_format(self):
-        assert qrcode.sanitize_target(target='+66-89-123-4567') == "66891234567"
+        assert qrcode.sanitize_target(target="+66-89-123-4567") == "66891234567"
 
 
 class TestFormatTarget:
@@ -54,8 +53,18 @@ class TestFormatAmount:
 
 class TestChecksum:
     def test_normal(self):
-        assert qrcode.checksum("00020101021129370016A000000677010111011300660000000005802TH53037646304") == "8956"
-        assert qrcode.checksum("00020101021129370016A000000677010111011300668999999995802TH53037646304") == "FE29"
+        assert (
+            qrcode.checksum(
+                "00020101021129370016A000000677010111011300660000000005802TH53037646304"
+            )
+            == "8956"
+        )
+        assert (
+            qrcode.checksum(
+                "00020101021129370016A000000677010111011300668999999995802TH53037646304"
+            )
+            == "FE29"
+        )
 
 
 class TestFormat:
@@ -66,71 +75,102 @@ class TestFormat:
 
 class TestGeneratePayload:
     def test_local_phone_number(self):
-        assert qrcode.generate_payload(
-            id='0801234567') == "00020101021129370016A000000677010111011300668012345675802TH530376463046197"
-        assert qrcode.generate_payload(
-            id='0899999999') == "00020101021129370016A000000677010111011300668999999995802TH53037646304FE29"
-        assert qrcode.generate_payload(
-            id='0891234567') == "00020101021129370016A000000677010111011300668912345675802TH5303764630429C1"
-        assert qrcode.generate_payload(
-            id='0000000000') == "00020101021129370016A000000677010111011300660000000005802TH530376463048956"
+        assert (
+            qrcode.generate_payload(id="0801234567")
+            == "00020101021129370016A000000677010111011300668012345675802TH530376463046197"
+        )
+        assert (
+            qrcode.generate_payload(id="0899999999")
+            == "00020101021129370016A000000677010111011300668999999995802TH53037646304FE29"
+        )
+        assert (
+            qrcode.generate_payload(id="0891234567")
+            == "00020101021129370016A000000677010111011300668912345675802TH5303764630429C1"
+        )
+        assert (
+            qrcode.generate_payload(id="0000000000")
+            == "00020101021129370016A000000677010111011300660000000005802TH530376463048956"
+        )
 
     def test_local_phone_number_with_dash(self):
-        assert qrcode.generate_payload(
-            id='080-123-4567') == "00020101021129370016A000000677010111011300668012345675802TH530376463046197"
+        assert (
+            qrcode.generate_payload(id="080-123-4567")
+            == "00020101021129370016A000000677010111011300668012345675802TH530376463046197"
+        )
 
     def test_local_phone_number_with_international_format(self):
-        assert qrcode.generate_payload(
-            id='+66-89-123-4567') == "00020101021129370016A000000677010111011300668912345675802TH5303764630429C1"
+        assert (
+            qrcode.generate_payload(id="+66-89-123-4567")
+            == "00020101021129370016A000000677010111011300668912345675802TH5303764630429C1"
+        )
 
     def test_national_id_number(self):
-        assert qrcode.generate_payload(
-            id='1111111111111') == "00020101021129370016A000000677010111021311111111111115802TH530376463047B5A"
-        assert qrcode.generate_payload(
-            id='1234567890123') == "00020101021129370016A000000677010111021312345678901235802TH53037646304EC40"
+        assert (
+            qrcode.generate_payload(id="1111111111111")
+            == "00020101021129370016A000000677010111021311111111111115802TH530376463047B5A"
+        )
+        assert (
+            qrcode.generate_payload(id="1234567890123")
+            == "00020101021129370016A000000677010111021312345678901235802TH53037646304EC40"
+        )
 
     def test_national_id_number_with_dash(self):
-        assert qrcode.generate_payload(
-            id='1-1111-11111-11-1') == "00020101021129370016A000000677010111021311111111111115802TH530376463047B5A"
+        assert (
+            qrcode.generate_payload(id="1-1111-11111-11-1")
+            == "00020101021129370016A000000677010111021311111111111115802TH530376463047B5A"
+        )
 
     def test_tax_id(self):
-        assert qrcode.generate_payload(
-            id='0123456789012') == "00020101021129370016A000000677010111021301234567890125802TH530376463040CBD"
+        assert (
+            qrcode.generate_payload(id="0123456789012")
+            == "00020101021129370016A000000677010111021301234567890125802TH530376463040CBD"
+        )
 
     def test_ewallet_id(self):
         # eWallet ID, KPlus ID
-        assert qrcode.generate_payload(
-            id='012345678901234') == "00020101021129390016A00000067701011103150123456789012345802TH530376463049781"
-        assert qrcode.generate_payload(
-            id='004999000288505') == "00020101021129390016A00000067701011103150049990002885055802TH530376463041521"
+        assert (
+            qrcode.generate_payload(id="012345678901234")
+            == "00020101021129390016A00000067701011103150123456789012345802TH530376463049781"
+        )
+        assert (
+            qrcode.generate_payload(id="004999000288505")
+            == "00020101021129390016A00000067701011103150049990002885055802TH530376463041521"
+        )
 
         # KPlus shop ID
-        assert qrcode.generate_payload(
-            id='004000006579718') == "00020101021129390016A00000067701011103150040000065797185802TH53037646304FBB5"
+        assert (
+            qrcode.generate_payload(id="004000006579718")
+            == "00020101021129390016A00000067701011103150040000065797185802TH53037646304FBB5"
+        )
 
     def test_amount_setting(self):
         # phone number
-        assert qrcode.generate_payload(
-            id='000-000-0000',
-            amount=4.22) == "00020101021229370016A000000677010111011300660000000005802TH530376454044.226304E469"
-        assert qrcode.generate_payload(
-            id='089-123-4567',
-            amount=13371337.75) == "00020101021229370016A000000677010111011300668912345675802TH5303764541113371337.756304B7D7"
+        assert (
+            qrcode.generate_payload(id="000-000-0000", amount=4.22)
+            == "00020101021229370016A000000677010111011300660000000005802TH530376454044.226304E469"
+        )
+        assert (
+            qrcode.generate_payload(id="089-123-4567", amount=13371337.75)
+            == "00020101021229370016A000000677010111011300668912345675802TH5303764541113371337.756304B7D7"
+        )
 
         # national id
-        assert qrcode.generate_payload(
-            id='1234567890123',
-            amount=420) == "00020101021229370016A000000677010111021312345678901235802TH53037645406420.006304BF7B"
+        assert (
+            qrcode.generate_payload(id="1234567890123", amount=420)
+            == "00020101021229370016A000000677010111021312345678901235802TH53037645406420.006304BF7B"
+        )
 
         # eWallet ID, KPlus ID
-        assert qrcode.generate_payload(
-            id='004999000288505',
-            amount=100.25) == "00020101021229390016A00000067701011103150049990002885055802TH53037645406100.256304369A"
+        assert (
+            qrcode.generate_payload(id="004999000288505", amount=100.25)
+            == "00020101021229390016A00000067701011103150049990002885055802TH53037645406100.256304369A"
+        )
 
         # KPlus shop ID
-        assert qrcode.generate_payload(
-            id='004000006579718',
-            amount=200.50) == "00020101021229390016A00000067701011103150040000065797185802TH53037645406200.5063048A37"
+        assert (
+            qrcode.generate_payload(id="004000006579718", amount=200.50)
+            == "00020101021229390016A00000067701011103150040000065797185802TH53037645406200.5063048A37"
+        )
 
 
 class TestToImage:
