c92bf5891b344779184238e20aeee263520fad56
Nathachai Thongniran <inid3a@gmail.com>
2020-07-18 12:25:53 +0700

    style, add comment

--- promptpay/qrcode.py
+++ promptpay/qrcode.py
@@ -1,6 +1,8 @@
 import re
 import crc16
 import qrcode
+from PIL import Image
+from typing import Any
 
 ID_PAYLOAD_FORMAT = "00"
 ID_POI_METHOD = "01"
@@ -22,13 +24,29 @@ TRANSACTION_CURRENCY_THB = "764"
 COUNTRY_CODE_TH = "TH"
 
 
-def sanitize_target(target=""):
+def sanitize_target(target: str = "") -> str:
+    """
+    Format target, allow only numeric character e.g.
+    - "012345678901234" => "012345678901234"
+    - "080-123-4567" => "0801234567"
+    - "1-1111-11111-11-1" => "1111111111111"
+    - "+66-89-123-4567" => "66891234567"
+
+    :param target:
+    :return:
+    """
     result = re.sub(r"\D", "", target)
 
     return result
 
 
-def format_target(target=""):
+def format_target(target: str = "") -> str:
+    """
+    todo complete docblockr
+
+    :param target:
+    :return:
+    """
     result = sanitize_target(target)
 
     if len(result) < 13:
@@ -38,13 +56,29 @@ def format_target(target=""):
     return result
 
 
-def format_amount(amount=0):
+def format_amount(amount: float = 0.00) -> str:
+    """
+    Convert to number with 2 decimal (string type) e.g.
+    - 10.23 => "10.23"
+    - 10 => "10.00"
+    - 1337.1337 => "1337.13"
+    - 1337.1387 => "1337.14"
+
+    :param amount:
+    :return:
+    """
     result = "{0:.2f}".format(amount)
 
     return result
 
 
-def checksum(target=""):
+def checksum(target: str = "") -> str:
+    """
+    todo complete docblockr
+
+    :param target:
+    :return:
+    """
     byte_str = target.encode("ascii")  # convert to bytes
     hex_str = hex(crc16.crc16xmodem(byte_str, 0xFFFF))
     code = hex_str.replace("0x", "").upper()
@@ -53,14 +87,28 @@ def checksum(target=""):
     return result
 
 
-def format(id="", value=""):
+def format(id: str = "", value: str = "") -> str:
+    """
+    todo complete docblockr
+
+    :param id:
+    :param value:
+    :return:
+    """
     last2digits = ("00" + str(len(value)))[-2:]
     result = id + last2digits + value
 
     return result
 
 
-def generate_payload(id="", amount=0):
+def generate_payload(id: str = "", amount: float = 0.00) -> str:
+    """
+    Generate payload for generate PromptPay QR code
+
+    :param id: PromptPay id e.g. national ID, phone number, eWallet, etc.
+    :param amount: number
+    :return:
+    """
     # sanitize id
     target = sanitize_target(id)
 
@@ -96,12 +144,25 @@ def generate_payload(id="", amount=0):
     return "".join(data)
 
 
-def to_image(payload=""):
+def to_image(payload: str = "") -> Image:
+    """
+    todo complete docblockr
+
+    :param payload: PromptPay Payload
+    :return:
+    """
     img = qrcode.make(payload)
+
     return img
 
 
-def to_file(payload="", filepath=""):
+def to_file(payload: str = "", filepath: str = "") -> None:
+    """
+
+    :param payload: PromptPay Payload
+    :param filepath: Target destination file path
+    :return:
+    """
     imgfile = open(filepath, "wb")
     img = to_image(payload)
     img.save(imgfile, "PNG")
