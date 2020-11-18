85ffd3c2ebe0b16dcb09b94d66cdafa837a25b59
Nathachai Thongniran <inid3a@gmail.com>
2020-11-18 16:22:38 +0700

    build(pypi): release version 1.1.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="promptpay",
-    version="1.1.5",
+    version="1.1.6",
     description="Python library to generate PromptPay QR Code",
     long_description=README,
     long_description_content_type="text/markdown",
